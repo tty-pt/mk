70ed9865d9d4954474f67fd87412e94337abac10
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-05-30 22:30:36 +0100

    Comment out runtime linking just as an experiment

--- include.mk
+++ include.mk
@@ -1,8 +1,9 @@
 npm-prefix := ${npm-lib:%=${npm-root}/%} \
 	${npm-lib:%=${npm-root-dir}/../../%}
 prefix := ${pwd} /usr/local
-CFLAGS := -g -Wall -Wextra -Wpedantic \
-	  ${npm-prefix:%=-I%/include}
+XCOMPILER := -Wall -Wextra -Wpedantic
+CFLAGS := -g ${npm-prefix:%=-I%/include}
+# LDFLAGS  :=  ${prefix:%=-Wl,-rpath,%/lib}
+# 	   ${npm-prefix:%=-L%/lib} \
 LDFLAGS	:= ${prefix:%=-L%/lib} \
-	   ${npm-prefix:%=-L%/lib} \
-	   ${npm-prefix:%=-Wl,-rpath,%/lib}
+	   ${npm-prefix:%=-L%/lib}
