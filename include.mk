a1b5156c8972e760f6aae0f2f7b8f5db905fdc37
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-10-02 08:36:53 +0100

    Build dirs

--- include.mk
+++ include.mk
@@ -1,41 +1,33 @@
 PREFIX ?= /usr/local
 RELDIR := .
 pwd != pwd
-npm-root != npm root
-npm-root-dir != dirname ${npm-root}
-prefix := ${pwd} \
-	  /usr/local \
-	  ${npm-lib:%=${npm-root}/%} \
-	  ${npm-lib:%=${npm-root-dir}/../../%}
+prefix := ${pwd} ${DESTDIR}${PREFIX}
 WARN := -Wall -Wextra -Wpedantic
 LDLIBS += ${LIB:%=-l%} ${LIB-LDLIBS}
 CFLAGS += ${prefix:%=-I%/include} ${WARN}
 LIB-LDFLAGS += ${prefix:%=-L%/lib}
 LDFLAGS	+= ${LIB-LDFLAGS} ${prefix:%=-Wl,-rpath,%/lib}
+ONELIB != echo ${LIB} | awk '{print $$1}'
+
 HEADERS += ${ONELIB:%=%.h}
 
 bintarget := ${BIN:%=bin/%} ${INSTALL-BIN:%=bin/%}
 libtarget := ${LIB:%=lib/lib%.so}
 
-ONELIB != echo ${LIB} | awk '{print $$1}'
-
 .SUFFIXES: .so .c .o
 
 all: ${libtarget} ${bintarget}
+	@echo ${installed-headers}
 
 ${bintarget}: ${libtarget} bin ${bintarget:bin/%=src/%.c}
-	@echo CC -o $@ ${@:bin/%=src/%.c} CFLAGS LDFLAGS ${LDLIBS}
-	@${CC} -o $@ ${@:bin/%=src/%.c} ${CFLAGS} ${LDFLAGS} ${LDLIBS}
+	${CC} -o $@ ${@:bin/%=src/%.c} ${CFLAGS} ${LDFLAGS} ${LDLIBS}
 
 ${libtarget}: ${LIB:%=src/lib%.c} ${HEADERS:%=include/%} lib
-	@echo CC -o $@ ${@:lib/%.so=src/%.c} CFLAGS \
-		-fPIC -shared LIB-LDFLAGS ${LIB-LDLIBS}
-	@${CC} -o $@ ${@:lib/%.so=src/%.c} ${CFLAGS} -fPIC \
+	${CC} -o $@ ${@:lib/%.so=src/%.c} ${CFLAGS} -fPIC \
 		-shared ${LIB-LDFLAGS} ${LIB-LDLIBS}
 
 .c.o:
-	echo CC -c -o ${@:%=${RELDIR}/%} CFLAGS ${<:%=${RELDIR}/%}
-	@${CC} -c -o ${@:%=${RELDIR}/%} ${CFLAGS} ${<:%=${RELDIR}/%}
+	${CC} -c -o ${@:%=${RELDIR}/%} ${CFLAGS} ${<:%=${RELDIR}/%}
 
 lib bin $(dirs):
 	@mkdir $@ 2>/dev/null || true
@@ -46,24 +38,30 @@ clean:
 installed-lib := $(LIB:%=${DESTDIR}${PREFIX}/lib/lib%.so)
 installed-pc := ${ONELIB:%=${DESTDIR}${PREFIX}/lib/pkgconfig/%.pc}
 
-$(installed-lib): ${libtarget}
+$(installed-lib): ${libtarget} ${DESTDIR}${PREFIX}/lib
 	install -m 644 ${@:${DESTDIR}${PREFIX}/%=%} \
 		${DESTDIR}${PREFIX}/lib
 
-$(installed-pc): ${ONELIB:%=%.pc}
+install-dirs := ${DESTDIR}${PREFIX}/lib ${DESTDIR}${PREFIX}/bin \
+	${DESTDIR}${PREFIX}/include ${DESTDIR}${PREFIX}/lib/pkgconfig
+
+$(install-dirs):
+	install -d $@
+
+$(installed-pc): ${ONELIB:%=%.pc} ${DESTDIR}${PREFIX}/lib/pkgconfig
 	install -m 644 \
 		${@:${DESTDIR}${PREFIX}/lib/pkgconfig/%=%} \
 		$(DESTDIR)${PREFIX}/lib/pkgconfig
 
 installed-headers := ${HEADERS:%=${DESTDIR}${PREFIX}/include/%}
 
-$(installed-headers): ${HEADERS:%=include/%}
+$(installed-headers): ${HEADERS:%=include/%} ${DESTDIR}${PREFIX}/include
 	install -m 644 ${@:${DESTDIR}${PREFIX}/%=%} \
 		$(DESTDIR)${PREFIX}/include
 
 installed-bin := $(INSTALL-BIN:%=$(DESTDIR)$(PREFIX)/bin/%)
 
-$(installed-bin): ${bintarget}
+$(installed-bin): ${bintarget} ${DESTDIR}${PREFIX}/bin
 	install -m 755 ${@:$(DESTDIR)$(PREFIX)/%=%} \
 		$(DESTDIR)${PREFIX}/bin
 
