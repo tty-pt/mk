246c11f16a3e07fe6fa54157a5f462e1f61fe995
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-10-02 08:36:53 +0100

    Build dirs

--- include.mk
+++ include.mk
@@ -1,72 +1,121 @@
-PREFIX ?= /usr/local
+# PREFIX ?= /usr/local
 RELDIR := .
+pwd := $(shell pwd)
 pwd != pwd
-npm-root != npm root
-npm-root-dir != dirname ${npm-root}
-prefix := ${pwd} \
-	  /usr/local \
-	  ${npm-lib:%=${npm-root}/%} \
-	  ${npm-lib:%=${npm-root-dir}/../../%}
+uname := $(shell test "${cross}" = "" && uname || echo ${cross})
+uname != test "${cross}" = "" && uname || echo ${cross}
+arch := $(shell uname -m)
+arch != uname -m
+prefix-Darwin-arm64  := /opt/homebrew
+prefix-Darwin-x86_64 := /usr/local
+prefix-Darwin += ${prefix-Darwin-${arch}}
+prefix-Linux := /usr
+prefix-OpenBSD := /usr/local
+prefix-Msys := /mingw64
+PREFIX ?= ${prefix-${uname}}
+DESTDIR     ?=
+prefix := ${DESTDIR}/${prefix-${uname}}
+cc-Linux := ${CC}
+cc-Darwin := ${CC}
+cc-OpenBSD := ${CC}
+cc-Msys := /usr/bin/x86_64-w64-mingw32-gcc
+cc := ${cc-${uname}}
+prefix := ${pwd} ${prefix-${uname}} ${PREFIX}
 WARN := -Wall -Wextra -Wpedantic
+distro != cat /etc/os-release | grep ID \
+	| sed 's/.*=//' || true
+# LDLIBS-Msys := -lbsd
+LDLIBS-alpine := -lbsd
+LIB-LDLIBS += ${LDLIBS-${uname}} ${LDLIBS-${distro}}
 LDLIBS += ${LIB:%=-l%} ${LIB-LDLIBS}
-CFLAGS += ${prefix:%=-I%/include} ${WARN}
+SO-Msys := dll
+SO-Linux := so
+SO-OpenBSD := so
+SO-Darwin := so
+SO := ${SO-${uname}}
+CFLAGS-BIN-Msys += -static
+# CFLAGS-LIB-Msys += SO=exe
+INCFLAGS += ${prefix:%=-I%/include} ${WARN} ${CFLAGS-${uname}} ${CFLAGS}
 LIB-LDFLAGS += ${prefix:%=-L%/lib}
 LDFLAGS	+= ${LIB-LDFLAGS} ${prefix:%=-Wl,-rpath,%/lib}
-HEADERS += ${ONELIB:%=%.h}
-
-bintarget := ${BIN:%=bin/%} ${INSTALL-BIN:%=bin/%}
-libtarget := ${LIB:%=lib/lib%.so}
-
+ONELIB := $(shell echo ${LIB} | awk '{print $$1}')
 ONELIB != echo ${LIB} | awk '{print $$1}'
 
-.SUFFIXES: .so .c .o
+FOLDER ?= ttypt
 
-all: ${libtarget} ${bintarget}
+HEADERS += ${ONELIB:%=${FOLDER}/%.h}
+
+bintarget := ${BIN:%=bin/%} ${INSTALL-BIN:%=bin/%}
+libtarget := ${LIB:%=lib/lib%.${SO}}
+
+.SUFFIXES: .${SO} .c .o
+
+all: info ${libtarget} ${bintarget}
+	@echo ${installed-headers}
+
+info:
+	@echo ARCH ${arch}
 
 ${bintarget}: ${libtarget} bin ${bintarget:bin/%=src/%.c}
-	@echo CC -o $@ ${@:bin/%=src/%.c} CFLAGS LDFLAGS ${LDLIBS}
-	@${CC} -o $@ ${@:bin/%=src/%.c} ${CFLAGS} ${LDFLAGS} ${LDLIBS}
+	${cc} -o $@ ${@:bin/%=src/%.c} \
+		${CFLAGS-BIN-${uname}} \
+		${INCFLAGS} ${LDFLAGS} ${LDLIBS}
 
 ${libtarget}: ${LIB:%=src/lib%.c} ${HEADERS:%=include/%} lib
-	@echo CC -o $@ ${@:lib/%.so=src/%.c} CFLAGS \
-		-fPIC -shared LIB-LDFLAGS ${LIB-LDLIBS}
-	@${CC} -o $@ ${@:lib/%.so=src/%.c} ${CFLAGS} -fPIC \
+	${cc} -o $@ ${@:lib/%.${SO}=src/%.c} ${INCFLAGS} \
+		${CFLAGS-LIB-${uname}} -fPIC \
 		-shared ${LIB-LDFLAGS} ${LIB-LDLIBS}
 
 .c.o:
-	echo CC -c -o ${@:%=${RELDIR}/%} CFLAGS ${<:%=${RELDIR}/%}
-	@${CC} -c -o ${@:%=${RELDIR}/%} ${CFLAGS} ${<:%=${RELDIR}/%}
+	${cc} -c -o ${@:%=${RELDIR}/%} ${INCFLAGS} ${<:%=${RELDIR}/%}
 
 lib bin $(dirs):
 	@mkdir $@ 2>/dev/null || true
 
 clean:
-	@rm lib/*.so bin/* src/*.o 2>/dev/null || true
+	@rm lib/*.${SO} bin/* src/*.o 2>/dev/null || true
 
-installed-lib := $(LIB:%=${DESTDIR}${PREFIX}/lib/lib%.so)
+installed-lib-Msys := $(LIB:%=${DESTDIR}${PREFIX}/bin/lib%.${SO})
+installed-lib := $(LIB:%=${DESTDIR}${PREFIX}/lib/lib%.${SO})
+
 installed-pc := ${ONELIB:%=${DESTDIR}${PREFIX}/lib/pkgconfig/%.pc}
 
-$(installed-lib): ${libtarget}
+$(installed-lib): ${libtarget} ${DESTDIR}${PREFIX}/lib
 	install -m 644 ${@:${DESTDIR}${PREFIX}/%=%} \
-		${DESTDIR}${PREFIX}/lib
+		${DESTDIR}${PREFIX}/lib/
 
-$(installed-pc): ${ONELIB:%=%.pc}
+$(installed-lib-Msys): ${libtarget} ${DESTDIR}${PREFIX}/bin
+	install -m 644 ${@:${DESTDIR}${PREFIX}/bin/%=lib/%} \
+		${DESTDIR}${PREFIX}/bin/
+
+install-dirs := ${DESTDIR}${PREFIX}/lib ${DESTDIR}${PREFIX}/bin \
+	${DESTDIR}${PREFIX}/include \
+	${DESTDIR}${PREFIX}/include/${FOLDER} \
+	${DESTDIR}${PREFIX}/lib/pkgconfig
+
+$(install-dirs):
+	install -d $@
+
+$(installed-pc): ${ONELIB:%=%.pc} ${DESTDIR}${PREFIX}/lib/pkgconfig
 	install -m 644 \
 		${@:${DESTDIR}${PREFIX}/lib/pkgconfig/%=%} \
 		$(DESTDIR)${PREFIX}/lib/pkgconfig
 
 installed-headers := ${HEADERS:%=${DESTDIR}${PREFIX}/include/%}
 
-$(installed-headers): ${HEADERS:%=include/%}
+$(installed-headers): ${HEADERS:%=include/%} ${DESTDIR}${PREFIX}/include/${FOLDER}
 	install -m 644 ${@:${DESTDIR}${PREFIX}/%=%} \
-		$(DESTDIR)${PREFIX}/include
+		$(DESTDIR)${PREFIX}/include/${FOLDER}
 
 installed-bin := $(INSTALL-BIN:%=$(DESTDIR)$(PREFIX)/bin/%)
 
-$(installed-bin): ${bintarget}
+$(installed-bin): ${bintarget} ${DESTDIR}${PREFIX}/bin
 	install -m 755 ${@:$(DESTDIR)$(PREFIX)/%=%} \
 		$(DESTDIR)${PREFIX}/bin
 
-install: $(installed-lib) $(installed-pc) $(installed-headers) $(installed-bin)
+install: ${installed-bin} ${installed-lib-${uname}} $(installed-lib) $(installed-pc) $(installed-headers)
 
-.PHONY: all clean install
+uninstall:
+	rm -rf ${installed-lib} ${installed-pc} ${installed-headers} ${installed-bin}
+
+.PHONY: all clean install uninstall
